1fc25904a3e4ecf7f75c2b80066dccd8b7a0370d
Nick <nikitaromanetskiy@gmail.com>
2024-01-19 22:32:40 +0200

    Problem 91

--- Problem_91/problem_91.py
+++ Problem_91/problem_91.py
@@ -1,8 +1,18 @@
 import time
 from math import gcd
+from typing import Union
 
 
-def right_triangles(size):
+def right_triangles(size: Union[int, float]) -> Union[int, float]:
+    """
+    Function to calculate the number of right triangles with sides lying on a grid of size 'size' x 'size'.
+    
+    Parameters:
+        size (int, float): The size of the grid.
+
+    Returns:
+        triangles (int, float): The number of right triangles.
+    """
     triangles = 3 * size * size
     for x in range(1, size + 1):
         for y in range(1, size + 1):
@@ -15,4 +25,3 @@ if __name__ == "__main__":
     start_time = time.time()
     print(f"Answer = {right_triangles(50)}")
     print(f"Elapsed time: {time.time() - start_time:.2f} seconds")
-    
